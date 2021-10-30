12fc098189c6c4652feb176a9044998adb20c088
Magnus <66132906+Magnus2727@users.noreply.github.com>
2021-10-30 14:08:40 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function execute_confirm(how_many){
+		for(var j=0;j<how_many;j++){
+			document.getElementsByClassName("list-group-item list-group-item-action lh-tight user-select-none unread ng-star-inserted")[0].click();
+			await sleep(150);
+			document.getElementsByClassName("btn btn-outline-success btn-sm px-lg-4 ng-star-inserted")[0].click();
+			await sleep(700);
+		}
+}	
+
+function confirm_messages(){
+		alert("dziala");
+		var element = document.getElementsByClassName("badge rounded-pill bg-primary float-end ng-star-inserted")[1];
+		var count = element.textContent;
+		execute_confirm(count);
+		
+}
+confirm_messages();
